200f1a921732ec1cc12a4b28536ab9c1a67043f0
LeDatTruongAn <msiggs09@gmail.com>
2017-12-15 16:02:31 +0700

    + Source của em

---
+++ SourceHocVien/KhungLong/Source/Classes/CustomSound.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "cocos2d.h"
+#include "SimpleAudioEngine.h"
+
+class CustomSound
+{
+public:
+	CustomSound();
+	~CustomSound();
+	
+	static void playEff(const char* path);
+
+private:
+
+};
---
+++ SourceHocVien/KhungLong/Source/Classes/GameCommon.h
@@ -0,0 +1,4 @@
+#pragma once
+extern float DW;
+
+extern float DH;
